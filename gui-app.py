2bf26a834f63f56019014d7b2f7b22c38dc52806
SBhat2615 <siddharthabhat09@gmail.com>
2023-02-19 17:48:39 +0530

    .

--- gui-app.py
+++ gui-app.py
@@ -8,16 +8,22 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg='#80c1ff')
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.5)
+frame1 = tk.Frame(root, bg='#80c1ff', bd=5)
+frame1.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-button = tk.Button(frame, text="Test button")
-button.place(relx=0, rely=0, relwidth=0.25, relheight=0.25)
+entry = tk.Entry(frame1, font=40)
+entry.place(relwidth=0.65, relheight=1)
 
-label = tk.Label(frame, text="This is a Label", bg='yellow', fg='red')
-label.place(relx=0.3, rely=0, relwidth=0.45, relheight=0.25)
+button = tk.Button(frame1, text="Test button", font=40)
+button.place(relx=0.7, relwidth=0.3, relheight=1)
 
-entry = tk.Entry(frame, bg='green')
-entry.place(relx=0.8, rely=0, relwidth=0.2, relheight=0.1)
+# frame2 = tk.Frame(root, bg='#80c1ff')
+# frame2.place(relx=0.2, rely=0.3, relwidth=0.6, relheight=0.5)
+
+# label = tk.Label(frame, text="This is a Label", bg='yellow', fg='red')
+# label.place(relx=0.3, rely=0, relwidth=0.45, relheight=0.25)
+
+# entry = tk.Entry(frame, bg='green')
+# entry.place(relx=0.8, rely=0, relwidth=0.2, relheight=0.1)
 
 root.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+entry = tk.Entry(frame1, font=40)
+entry.place(relwidth=0.65, relheight=1)
